6c583585a6b925224c72c382d236b2c92c018cdc
Stephen LM <engineering@finbourne.com>
2021-01-15 11:56:29 +0000

    SE-679: checkpoint

--- sdk/tests/test_finbourne_access.py
+++ sdk/tests/test_finbourne_access.py
@@ -2,13 +2,16 @@ import unittest
 from finbourne_access import api as sa
 from finbourne_access.utilities import ApiClientFactory
 
-api_client = ApiClientFactory()
-policies_api = api_client.build(sa.PoliciesApi)
 
 class FinbourneAccessTests(unittest.TestCase):
 
+    @classmethod
+    def setUpClass(cls):
+        api_client = ApiClientFactory()
+        cls.policies_api = api_client.build(sa.PoliciesApi)
+
     def test_roles(self):
-        policies = policies_api.list_policies()
+        policies = self.policies_api.list_policies()
         self.assertGreater(len(policies), 0)
 
 
