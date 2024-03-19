53f05d57d86da5414f82f523e0224e22b032c53a
Concourse <concourse@finbourne.com>
2024-03-19 16:55:18 +0000

    Updates for v2.0.121

--- sdk/finbourne_access/api/__init__.py
+++ sdk/finbourne_access/api/__init__.py
@@ -7,3 +7,11 @@ from finbourne_access.api.policy_templates_api import PolicyTemplatesApi
 from finbourne_access.api.roles_api import RolesApi
 from finbourne_access.api.user_roles_api import UserRolesApi
 
+
+__all__ = [
+    "ApplicationMetadataApi",
+    "PoliciesApi",
+    "PolicyTemplatesApi",
+    "RolesApi",
+    "UserRolesApi"
+]
--- sdk/finbourne_access/extensions/__init__.py
+++ sdk/finbourne_access/extensions/__init__.py
@@ -5,3 +5,14 @@ from finbourne_access.extensions.configuration_loaders import (
     EnvironmentVariablesConfigurationLoader,
     ArgsConfigurationLoader,
 )
+from finbourne_access.extensions.api_client import SyncApiClient
+
+__all__ = [
+    "SyncApiClientFactory",
+    "ApiClientFactory",
+    "ConfigurationLoader",
+    "SecretsFileConfigurationLoader",
+    "EnvironmentVariablesConfigurationLoader",
+    "ArgsConfigurationLoader",
+    "SyncApiClient"
+]
