1a7ab6c651fcdf904e011f7a0a13da049f9022c6
Concourse <concourse@finbourne.com>
2024-11-11 18:20:02 +0000

    access python updates for v2.1.181
    Signed-off-by: Concourse <concourse@finbourne.com>

--- sdk/finbourne_access/configuration.py
+++ sdk/finbourne_access/configuration.py
@@ -445,7 +445,7 @@ class Configuration:
         return "Python SDK Debug Report:\n"\
                "OS: {env}\n"\
                "Python Version: {pyversion}\n"\
-               "Version of the API: 0.0.4003\n"\
+               "Version of the API: 0.0.4004\n"\
                "SDK Package Version: {package_version}".\
                format(env=sys.platform, pyversion=sys.version, package_version=package_version)
 
